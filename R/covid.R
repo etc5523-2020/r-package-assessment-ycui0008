46758dd4ca122d7daa51abe42879db3e655bdc86
Yuheng Cui <ycui0008@student.monash.edu>
2020-10-20 13:29:10 +1100

    Complete documentation for covid dataset

--- R/covid.R
+++ R/covid.R
@@ -3,22 +3,21 @@
 #' @description
 #' This dataset contains covid-19 cases and deaths of 225 countries. The date of the dataset is 2020-10-06, originally.
 #'
-#' @param
-#' date_reported
-#' country_code country
-#' country country name
-#' who_region
-#' new_cases
-#' cumulative_cases
-#' new_deaths
-#' cumulative_deaths
-#'
+#' @format  A data frame with columns:
+#' \describe{
+#'  \item{date_reported}{Date of reported cases and deaths.}
+#'  \item{country_code}{Unique code for countries.}
+#'  \item{country}{Full country names.}
+#'  \item{who_region}{Region defined by WHO.}
+#'  \item{new_cases, new_deaths}{New cases/deaths in last 24 hours.}
+#'  \item{cumulative_cases, cumulative_deaths}{Total number of cases and deaths.}
+#' }
 #'
 #'
 #'
 #' @source
 #' World Health Organisation (WHO)
-#' https://covid19.who.int/WHO-COVID-19-global-data.csv
+#' <https://covid19.who.int/WHO-COVID-19-global-data.csv>, downloaded 2020-10-06
 #'
 #'
-
+"covid"
