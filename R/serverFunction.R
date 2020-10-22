19dd4dd5907d103fed882fc74519c2888b91f883
Yuheng Cui <ycui0008@student.monash.edu>
2020-10-22 11:35:30 +1100

    try to pass CMD check, 1st time

--- R/serverFunction.R
+++ R/serverFunction.R
@@ -14,20 +14,19 @@
 #'
 #' @import ggplot2
 #' @importFrom stats reorder
-#' @importFrom stats filter
+#' @importFrom dplyr filter
 #' @importFrom utils head
 #'
-#' @examples
-#' colPlot(data = covid, date = input$end_date2, color = 'red', varName = cumulative_deaths, title = "Cumulative deaths")
 #'
 #' @export
 colPlot <- function(data, date, color, varName, title){
   data %>%
-    filter(date_reported == {{date}}) %>%
-    arrange(desc({{varName}})) %>%
-    head(5) %>%
+    dplyr::filter(date_reported == {{date}}) %>%
+    dplyr::arrange(desc({{varName}})) %>%
+    utils::head(5) %>%
     ggplot2::ggplot(aes(x = reorder(country, {{varName}}), y = {{varName}})) +
     geom_col(fill = color) + #color must be character
     ggplot2::ggtitle(title) + # as well as title
     coord_flip()
 }
+
